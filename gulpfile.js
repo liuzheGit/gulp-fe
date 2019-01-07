274947d9b2cc24d6bc93545fd58eebf319e6e12a
liuzhe <zhethink@gmail.com>
2019-01-07 16:24:25 +0800

    定制化gulp工作流(简单的)

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const gulp = require('gulp');
+
+// babel 转 js
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+//  压缩js
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+// scss转css  和 压缩
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+// 压缩图片
+const tinypng = require('gulp-tinypng');
+const myKey = 'EEr7GcvNBNDyUVEfkgX1jYpUtQ6eGkH6';
+
+// watch   scss  转 css
+gulp.task('sass', function(){
+   return gulp.src('src/scss/*.scss')
+    .pipe(sass(''))
+    .pipe(gulp.dest('dist/css'));
+})
+
+gulp.task('watch', function(){
+  gulp.watch('src/scss/*.scss', gulp.series('sass'));
+})
+
+// 压缩css
+gulp.task('mincss', () => 
+  gulp.src('dist/css/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/min-css'))
+)
+
+//  babel 转码js  和 压缩js  并且有source map
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('src/js/*.js'),
+        sourcemaps.init(),
+        babel({
+          presets: ['@babel/env']
+        }),
+        uglify(),
+        sourcemaps.write(),
+        gulp.dest('dist/js')
+    ],
+     cb
+  );
+});
+
+//  压缩图片
+
+gulp.task('tinypng', () =>
+  gulp.src('src/images/*')
+    .pipe(tinypng(myKey))
+    .pipe(gulp.dest('dist/images2'))
+)
+
+
+
+
+
+// gulp.task('default', () =>{
+//   gulp.src('src/app.js')
+//     .pipe(sourcemaps.init())
+//     .pipe(babel({
+//       presets: ['@babel/env']
+//     }))
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest('dist'))
+// })
---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+function Person(obj){
+  this.name = obj.name;
+  this.age = obj.age;
+}
+
+
+Person.prototype.say = function(){
+  console.log('myName: ', this.name);
+}
+
+
+const L = new Person({name: 'ldifd', age: 18})
+
+
+
+L.say();
+
+console.lo('fdjkfd')
