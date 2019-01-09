6f9aeca9b4ecc34b7f4df2d1226cfaccac0a1815
liuzhe <zhethink@gmail.com>
2019-01-09 10:43:02 +0800

    实战

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,1 @@
-function Person(obj){
-  this.name = obj.name;
-  this.age = obj.age;
-}
-
-
-Person.prototype.say = function(){
-  console.log('myName: ', this.name);
-}
-
-
-const L = new Person({name: 'ldifd', age: 18})
-
-let url = 'https://jsonplaceholder.typicode.com/todos/2';
-fetch(url).then( res => res.json())
-  .then(json => {
-    console.log(json);
-  })
+console.log('done')
