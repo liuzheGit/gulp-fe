3db5426c39520692b7267b5aa8b39eda19585442
liuzhe <zhethink@gmail.com>
2019-01-09 17:51:01 +0800

    完成底部区域

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,33 @@
   // //   var $index = $(this).index();
   // //   $('.content-main').eq($index).toggleClass('active');
   // })
+
+  $(function(){
+    var $pink = '#FA6CB0';
+    var $red = '#FE0000';
+    var $orange = '#FA6CB0';
+    var $yellow = '#FFFF01';
+    var $cyan = '#3FE0D0';
+    var $blue = '#400097';
+    var $purple = '#a874e1';
+    var colorArr = [$pink, $red, $orange, $yellow, $cyan, $blue, $purple];
+
+
+    $('.changeColor li').click(function(){
+      var $index = $(this).index();
+      var $color = colorArr[$index];
+      console.log($color);
+    });
+
+    $('.params .params-title').click(function(){
+      var $index = $(this).index();
+      var $target = $(this).next();
+      if($target.hasClass('active')){
+        $target.slideUp();
+        $target.removeClass('active');
+      }else{
+        $target.slideDown();
+        $target.addClass('active');
+      }
+    })
+  })
