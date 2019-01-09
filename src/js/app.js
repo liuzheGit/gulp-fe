f5ca3e5239ccd482a318e7bc62ac02117cf044d3
liuzhe <zhethink@gmail.com>
2019-01-09 15:13:24 +0800

    除了下部,其他铺完, 现在加自定义的range

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-console.log('done')
+  // $('.params-content .params-title').on('click',function(){
+  //   console.log($(this));
+  // //   if($(this).find('.icon-left')){
+  // //     $(this).find('.icon-left').removeClass('icon-left').addClass('icon-left1')
+  // //   }
+  // //   if($(this).find('.icon-left1')){
+  // //     $(this).find('.icon-left1').removeClass('icon-left1').addClass('icon-left')
+  // //   }
+  // //   var $index = $(this).index();
+  // //   $('.content-main').eq($index).toggleClass('active');
+  // })
